7f757c4b228b194cc474abce909b475eaf3f7a2c
jepetro <97709211+jepetro@users.noreply.github.com>
2022-04-27 23:58:59 -0400

    updated

---
+++ itis3135/project/scripts/nav.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    var x = document.getElementById("mytopnav");
+    if(x.className === "topnav")
+    {
+        x.className += " responsive";
+    }
+    else
+    {
+        x.className = "topnav";
+    }
+}
